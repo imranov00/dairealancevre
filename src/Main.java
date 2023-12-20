e10f4e03316908be9ec26612051028c970cc39b6
İmranov <semtexpert@gmail.com>
2023-12-20 16:00:32 +0300

    alan cevre hesaplama

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double r,alan, a, pi=3.14;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yarıçap giriniz : ");
+        r=scanner.nextDouble();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Merkez Açısını Giriniz: ");
+        a=scanner1.nextDouble();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        alan=(pi*(r*r)*a)/360;
+        System.out.println("Dairenin Alanı :" + alan);
+
+
+
         }
+
     }
-}
